5f802ff5c1bb524a9762849ec4f553858aca527d
KeshabPoudel <keshabpoudel07@outlook.com>
2022-12-14 17:14:06 -0500

    Revert "dsfsdf"
    This reverts commit f0f650af3f45aae5e2b13e1cd9a0f577e12b7e61, reversing
    changes made to 1ec993a8e48340ca94adddde13951fb06b8ad7d3.

--- amr/assets/js/filter.js
+++
@@ -1,9 +0,0 @@
-let Price = [
-    {name: 'ACURA ILX', price: 19483},
-    {name: 'Acura TLX', price: 26944},
-    {name: 'Audi Q5', price: 36538},
-    {name: 'BMW X4', price: 39998},
-    {name: 'BMW 3 Series', price: 18619},
-    {name: 'Chevrolet Blazer', price: 27600}
-
-]
