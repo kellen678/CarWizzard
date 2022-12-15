f83184eebb924c893201e9fc82cc7a4fb97f0757
kellen678 <Kellencm.barr@gmail.com>
2022-12-15 09:22:31 -0500

    Added filter

--- amr/assets/js/filter.js
+++ amr/assets/js/filter.js
@@ -5,5 +5,30 @@ let Price = [
     {name: 'BMW X4', price: 39998},
     {name: 'BMW 3 Series', price: 18619},
     {name: 'Chevrolet Blazer', price: 27600}
+]
+let Door = [
+    {name: 'ACURA ILX', doors: 1 },
+    {name: 'Acura TLX', doors: 1 },
+    {name: 'Audi Q5', doors: 1 },
+    {name: 'BMW X4', doors: 1 },
+    {name: 'BMW 3 Series', doors: 1 },
+    {name: 'Chevrolet Blazer', doors: 1 }
+]
 
+let Year = [
+    {name: 'ACURA ILX', year: 1 },
+    {name: 'Acura TLX', year: 1 },
+    {name: 'Audi Q5', year: 1 },
+    {name: 'BMW X4', year: 1 },
+    {name: 'BMW 3 Series', year: 1 },
+    {name: 'Chevrolet Blazer', year: 1 }
+]
+
+let Condition = [
+    {name: 'ACURA ILX', doors: 1 },
+    {name: 'Acura TLX', doors: 1 },
+    {name: 'Audi Q5', doors: 1 },
+    {name: 'BMW X4', doors: 1 },
+    {name: 'BMW 3 Series', doors: 1 },
+    {name: 'Chevrolet Blazer', doors: 1 },
 ]
