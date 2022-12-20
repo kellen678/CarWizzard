de4e47743faa6f94218a1a4ebe7fb4a092e79c44
kellen678 <68203345+kellen678@users.noreply.github.com>
2022-12-20 16:50:05 -0500

    holy fuck the filter is working a bit

---
+++ CarWizard/assets/js/brandfilter.js
@@ -0,0 +1,21 @@
+
+function getSelectedValue() {
+    var selectValue = document.getElementById("make").value;
+    console.log(selectValue);
+    var acceptedValues = 
+    ['Acura', 'Audi', 'BMW', 'Chevrolet','Ford','Honda','Hyundai','Jeep','Kia','Nissan','Ram','Toyota','Volkswagen','Volvo','Mazda','Mercedes-Benz','Mini Coupe','Infiniti','Lexus',];
+    const productCards = document.querySelectorAll('.product-card');
+    console.log(productCards);
+  
+    productCards.forEach(card => {
+      if(acceptedValues.includes(selectValue)) {
+        if(card.classList.contains(selectValue)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      } else {
+        card.style.display = 'block';
+      }
+    })
+  }
