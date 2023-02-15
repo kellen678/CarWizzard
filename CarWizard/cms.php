eb39766599230d780a54f336805f5db24d968a63
kellen678 <68203345+kellen678@users.noreply.github.com>
2023-02-15 09:30:06 -0500

    ff

---
+++ CarWizard/cms.php
@@ -0,0 +1,19 @@
+<?php
+$Condition = $_POST['Condition'];
+$Make = $_POST['Make'];
+$Model = $_POST['Model'];
+$Drivetrain = $_POST['Drivetrain'];
+$Transmission = $_POST['Transmission'];
+$Mileage = $_POST['Mileage'];
+$FuelEfficency = $_POST['FuelEfficency'];
+$Engine = $_POST['Engine'];
+$Horsepower = $_POST['Horsepower'];
+$Year = $_POST['Year'];
+$Interior = $_POST['Interior'];
+$Exterior = $_POST{'Exteriro'};
+$Description = $_POST['Description'];
+$Extras = $_POST['Extras'];
+$Name = $_POST['Name'];
+$Images = $_POST['Images'];
+$Price = $_POST['Price'];
+?>
