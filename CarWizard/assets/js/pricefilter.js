470cd2b7bcfac4f0b6d67d3cbddc798aaae6f659
kellen678 <kellencm.barr@gmail.com>
2023-03-05 03:33:05 -0500

    cms

--- CarWizard/assets/js/pricefilter.js
+++ CarWizard/assets/js/pricefilter.js
@@ -1,29 +1,11 @@
 function getSelectedValuePrice() {
-  const pricevalue = document.getElementById("price").value;
-  const makevalue = document.getElementById("make").value;
-  const yearvalue = document.getElementById("year").value;
-  const acceptedValues = ['0to5', '5to10', '15to20', '20to25', '25to30','30to35','35to40', '35-40', '40+', 'Acura', 'Audi', 'BMW', 'Chevrolet','Ford','Honda','Hyundai','Jeep','Kia','Nissan','Ram','Toyota','Volkswagen','Volvo','Mazda','Mercedes-Benz','Mini Coupe','Infiniti','Lexus', '2022','2021','2020','2019','2018','2017','2016','2015','2014','2013','2012','2011','2009','2007'];
+  const selectValue = document.getElementById("price").value;
+  const acceptedValues = ['0to5', '5to10', '15to20', '20to25', '25to30','30to35','35to40','40+'];
   const productCards = document.querySelectorAll('.product-item');
 
   productCards.forEach(card => {
-    console.log(pricevalue, makevalue, yearvalue);
-
-    if (pricevalue === "Price" && makevalue === "Make" && yearvalue === "Year"){
-      console.log(card);
-      return
-    } else if (pricevalue != "Price" && makevalue != "Make"){
-
-    }else if (pricevalue != "Price"){
-
-    }else {
-
-    }
-  
-
-    
-
-    if(acceptedValues.includes(pricevalue || makevalue || yearvalue)) {
-      if(card.classList.contains(yearvalue) || card.classList.contains(pricevalue) || card.classList.contains(makevalue) ) {
+    if(acceptedValues.includes(selectValue)) {
+      if(card.classList.contains(selectValue)) {
         card.style.display = 'block';
         console.log(card);
       } else {
@@ -34,7 +16,45 @@ function getSelectedValuePrice() {
       card.style.display = 'block';
       console.log(card);
     }
+  })
+
+
+const selectValuemake = document.getElementById("make").value;
+const acceptedValuesmake = ['Acura', 'Audi', 'BMW', 'Chevrolet','Ford','Honda','Hyundai','Jeep','Kia','Nissan','Ram','Toyota','Volkswagen','Volvo','Mazda','Mercedes-Benz','Mini Coupe','Infiniti','Lexus'];
+const productCardsmake = document.querySelectorAll('.product-item');
+  
+    productCardsmake.forEach(card => {
+      if(acceptedValuesmake.includes(selectValuemake)) {
+        if(card.classList.contains(selectValuemake)) {
+          card.style.display = 'block';
+          console.log(card);
+        } else {
+          card.style.display = 'none';
+          console.log(card);
+        }
+      } else {
+        card.style.display = 'block';
+        console.log(card);
+      }
+    })
+
+    const selectValueprice = document.getElementById("year").value;
+    const acceptedValuesprice = ['2022','2021','2020','2019','2018','2017','2016','2015','2014','2013','2012','2011','2009','2007'];
+    const productCardsprice = document.querySelectorAll('.product-item');
+  
+    productCardsprice.forEach(card => {
+      if(acceptedValuesprice.includes(selectValueprice)) {
+        if(card.classList.contains(selectValueprice)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      } else {
+        card.style.display = 'block';
+      }
+    })
   }
-}
+  
+
 
 
